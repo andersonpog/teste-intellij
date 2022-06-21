c64592bbc0701327b3033772adc55907c0790812
andersonpog <silveira.anderson.1988@gmail.com>
2022-06-21 19:07:38 -0300

    Lista animais

--- src/PrimeiroTeste.java
+++ src/PrimeiroTeste.java
@@ -2,6 +2,9 @@ import model.Cachorro;
 import model.Gato;
 import model.Animal;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrimeiroTeste {
     public static void main(String[] args) {
         System.out.println("To aqui");
@@ -11,6 +14,11 @@ public class PrimeiroTeste {
         Cachorro tom = new Cachorro("Tom", "Caramelo", 10);
 
         tom.setIdade(15);
+
+        List<Animal> animais = new ArrayList<>();
+
+        animais.add(doido);
+        animais.add(tom);
 
         System.out.println(doido);
         System.out.println(tom);
