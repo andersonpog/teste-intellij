83398b8fa9172bac82602d8b0456c9b89f0b90c2
andersonpog <silveira.anderson.1988@gmail.com>
2022-06-21 17:59:08 -0300

    First commit

---
+++ src/PrimeiroTeste.java
@@ -0,0 +1,11 @@
+import model.Gato;
+
+public class PrimeiroTeste {
+    public static void main(String[] args) {
+        System.out.println("To aqui");
+
+        Gato doido = new Gato("Doido", "Branco", 3);
+
+        System.out.println(doido.toString());
+    }
+}
